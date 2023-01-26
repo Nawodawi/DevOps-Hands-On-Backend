3158d0a5cb424ee729ec26cfa42dfb7d91644944
Nawodawi <nawodawijebandara7@gmail.com>
2023-01-26 23:19:55 +0530

    GET all added

--- devops/backend/controllers/postsController.go
+++ devops/backend/controllers/postsController.go
@@ -9,9 +9,14 @@ import (
 
 func PostsCreate(c *gin.Context) {
 	//Get data off req body
+	var body struct {
+		Body  string
+		Title string
+	}
 
+	c.Bind(&body)
 	//Create a post
-	post := models.Post{Title: "Hello", Body: "this is a body"}
+	post := models.Post{Title: body.Title, Body: body.Body}
 
 	result := initializers.DB.Create(&post) // pass pointer of data to Create
 
@@ -24,4 +29,16 @@ func PostsCreate(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"post": post,
 	})
+}
+
+func PostsIndex(c *gin.Context) {
+	//Get posts
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+	//Response with them
+
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+
 }
--- devops/backend/main.go
+++ devops/backend/main.go
@@ -17,6 +17,7 @@ func main() {
 
 	r := gin.Default()
 	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 }
